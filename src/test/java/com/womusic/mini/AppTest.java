1528c31ceca1accbee646f7d84b6eec9d5f14eed
wuyongtao <wuyt33@chinaunicom.cn>
2019-06-17 09:34:50 +0800

    add test cases

--- src/test/java/com/womusic/mini/AppTest.java
+++ src/test/java/com/womusic/mini/AppTest.java
@@ -1,21 +1,36 @@
 package com.womusic.mini;
 
-//import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-//import org.junit.Test;
+import java.util.List;
 
 /**
- * Unit test for simple App.
+ * Unit test for minicode.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-  //  @Test
-    public void shouldAnswerWithTrue()
+    @Test
+    public void testStageOne()
     {
+        StageOneMapper stageOneMapper = new StageOneMapper();
+        Solution stageOneSolution = new Solution(stageOneMapper);
+        int[] testNumbers={2,1,9,3};
+        List<String> results = stageOneSolution.CalCombinationsByNumbers(testNumbers);
+        assertTrue( results.size() == 36 );
+        assertTrue( results.get(0).equals("awd"));
+        assertTrue( results.get(35).equals("czf"));
+    }
 
-        //assertTrue( true );
+    @Test
+    public void testStageTwo()
+    {
+        StageOneMapper stageTwoMapper = new StageTwoMapper();
+        Solution stageTwoSolution = new Solution(stageTwoMapper);
+        int[] testNumbers={1,20,3};
+        List<String> results = stageTwoSolution.CalCombinationsByNumbers(testNumbers);
+        assertEquals( 9, results.size());
+        assertEquals( "ad", results.get(0));
+        assertEquals( "cf", results.get(8));
     }
 }
